218af39c88d71ccdf7a89a999d9703b8a5576956
bholmesdev <bholmesdev@gmail.com>
2024-09-09 08:42:53 -0400

    deps: icons, drizzle, tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import icons from "unplugin-icons/vite";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    plugins: [
+      icons({
+        compiler: "astro",
+      }),
+      {
+        name: "configure-response-headers",
+        configureServer: (server) => {
+          server.middlewares.use((_req, res, next) => {
+            res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+            res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+            next();
+          });
+        },
+      },
+    ],
+    optimizeDeps: {
+      exclude: ["sqlocal"],
+    },
+  },
+});

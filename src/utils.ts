886c7af7ac17e93564a9eca2741942ef1bb0e6ed
bholmesdev <bholmesdev@gmail.com>
2024-09-10 08:17:07 -0400

    feat: styling

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,3 @@
-export type MaybePromise<T> = T | Promise<T>;
-
-export function viewTransition(cb: () => MaybePromise<void>) {
-	if ('startViewTransition' in document && typeof document.startViewTransition === 'function') {
-		document.startViewTransition(cb);
-		return;
-	}
-	cb();
-}
-
 export function getElement(id: string): HTMLElement {
 	const el = document.getElementById(id);
 	if (!el) {
